b339039f541e2f59c378c21bdd083ea7f98680d2
manicqin <8845802>
2016-04-12 15:06:51 +0300

    server handlers are working and using latest functionality

--- src/web_server/game_sessions_handler.rs
+++ src/web_server/game_sessions_handler.rs
@@ -1,16 +1,20 @@
 extern crate iron;
-//extern crate persistent;
-//extern crate bodyparser;
+extern crate persistent;
+extern crate bodyparser;
 extern crate plugin;
-//extern crate params;
+extern crate params;
 
 use iron::prelude::*;
 use iron::{Handler, status, BeforeMiddleware};
 use iron::typemap::Key;
+use self::params::*;
 use std::sync::Arc;
+
+use web_utils::server_errors::*;
 use game_logic::game_session::GameSession;
 //game_session::GameSession;
 
+#[derive(Debug)]
 pub struct GamSessions{
     registry : Vec<Arc<GameSession>>,
 }
@@ -27,6 +31,12 @@ impl GamSessions{
     pub fn push(&mut self,session: GameSession){
         self.registry.push(Arc::new(session));
     }
+
+    pub fn find_player_games(&self,player_id: u32)->Vec<&Arc<GameSession>>{
+        self.registry.iter()
+            .filter(|x| (**x).contains_player(&player_id))
+            .collect::<Vec<_>>()
+    }
 }
 impl Handler for GameSessionFinder{
     fn handle(&self, _:&mut Request)-> IronResult<Response>{
@@ -38,15 +48,37 @@ impl Handler for GameSessionFinder{
     }
 }
 
-
+// fn bla(req : & Request) ->  Params::Map {
+//     Params::Map::new()
+// }
+    
 impl BeforeMiddleware for GameSessionFinder {
-    fn before(&self, _: & mut Request) -> IronResult<()> {
+        
+    fn before(&self, req: & mut Request) -> IronResult<()> {
         println!("GameSessionFinder Before");
+       
+        let map = req.get::<Params>().unwrap();
+        
+        let lock = req.get::<persistent::State<GamSessions>>().unwrap();
+        let dynamic_asset = lock.read().unwrap();
+        let game_id : _;
+
+        match map.find(&["gameid"]) {
+            Some(&Value::String(ref id)) => game_id = id.parse::<u32>().unwrap_or(0),
+            _ => {
+                return Err(IronError::new(StringError("No gameid was given".to_string()), status::NotFound));},
+        }
+        
+        println!(",fhdskjfhjdfsh {:?}", *dynamic_asset);
+        for game in dynamic_asset.find_player_games(game_id).iter(){
+            println!("{:?}",game);
+        } 
+        
         Ok(())
     }
 
     fn catch(&self, _: &mut Request, err: IronError) -> IronResult<()> {
-        println!("GameSessionFinder BOOM MOTHERFUCKER");
+        println!("GameSessionFinder BOOM MOTHERFUCKER {:?}",err);
         Err(err)
     }
 }
--- src/web_server/players_handler.rs
+++ src/web_server/players_handler.rs
@@ -1,33 +1,21 @@
 extern crate iron;
-//extern crate persistent;
-//extern crate bodyparser;
+extern crate persistent;
+extern crate bodyparser;
 extern crate plugin;
-//extern crate params;
+extern crate params;
 
 use iron::prelude::*;
 use iron::{status, BeforeMiddleware};
 use iron::typemap::Key;
-//use params::{Params, Value};
+use self::params::*;
 
 use game_logic::player::Player;
 use std::collections::HashMap;
+use web_utils::server_errors::*;
 
 use std::error::Error;
 use std::fmt::{self, Debug};
 use std::sync::Arc;
-
-#[derive(Debug)]
-struct StringError(String);
-
-impl fmt::Display for StringError {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        Debug::fmt(self, f)
-    }
-}
-
-impl Error for StringError {
-    fn description(&self) -> &str { &*self.0 }
-}
 
 //Holds file and comments vector
 //loads file nad dump it into vector
@@ -103,30 +91,29 @@ pub struct PlayersHandlerAuthenticator;
 
 impl BeforeMiddleware for PlayersHandlerAuthenticator {
     fn before(&self, req: & mut Request) -> IronResult<()> {
+        println!("PlayersHandlerAuthenticator before");
 
-    //     let map = req.get_ref::<Params>().unwrap().clone();
-    //     let lock : & _ = req.get_ref::<persistent::State<PlayersHandler>>().unwrap();
-    //     let dynamic_asset = lock.read().unwrap();
-    //     let player_id : _;
-    //
-    //     match map.find(&["userid"]) {
-    //         Some(&Value::String(ref id)) => player_id = id.parse::<u32>().unwrap_or(0),
-    //         _ => panic!("Unexpected parameter type!"),
-    //     }
-    //
-    //     match dynamic_asset.contains(&player_id) {
-    //         true => Ok(()),
-    //         false => Err(IronError::new(StringError("No player id".to_string()), status::NotFound)),
-    //     }
-    //
-    //
-    //
-    // }
-    //
-    // fn catch(&self, req: &mut Request, err: IronError) -> IronResult<()> {
-    //     println!("PlayersHandlerAuthenticator BOOM MOTHERFUCKER");
-    //     Err(err)
-        Ok(())
+        let map = req.get_ref::<Params>().unwrap().clone();
+        let lock : & _ = req.get_ref::<persistent::State<PlayersHandler>>().unwrap();
+        let dynamic_asset = lock.read().unwrap();
+        let player_id : _;
+
+        match map.find(&["userid"]) {
+            Some(&Value::String(ref id)) => player_id = id.parse::<u32>().unwrap_or(0),
+            _ => {
+                return Err(IronError::new(StringError("No player id in param".to_string()), status::NotFound));},
+        }
+
+        println!("{:?}", player_id);
+        match dynamic_asset.contains(&player_id) {
+            true => Ok(()),
+            false => Err(IronError::new(StringError("No player id in roster".to_string()), status::NotFound)),
+        }
+    }
+
+    fn catch(&self, req: &mut Request, err: IronError) -> IronResult<()> {
+        println!("PlayersHandlerAuthenticator BOOM MOTHERFUCKER {:?}",err);
+        Err(err)
      }
 
 }
@@ -135,4 +122,25 @@ impl BeforeMiddleware for PlayersHandlerAuthenticator {
 pub fn auhentcate_player<'a>(req: &'a mut Request) -> IronResult<Response> {
     println!("auhentcate_player");
     Ok(Response::with((status::Ok, format!("{:?}",req))))
+}
+
+pub fn add_player<'a>(req: &'a mut Request) -> IronResult<Response> {
+    println!("add_player");
+
+    let map = req.get::<Params>().unwrap();
+    let lock = req.get::<persistent::State<PlayersHandler>>().unwrap();
+    let mut dynamic_asset = lock.write().unwrap();
+    let player_id : _;
+
+    match map.find(&["userid"]) {
+        Some(&Value::String(ref id)) => player_id = id.parse::<u32>().unwrap_or(0),
+        _ => panic!("Unexpected parameter type!"),
+    }
+
+    if dynamic_asset.contains(&player_id) {
+        return Err(IronError::new(StringError("Duplicate player id".to_string()), status::NotFound));
+    }
+
+    dynamic_asset.add_player(Player::new(player_id));
+    Ok(Response::with((status::Ok, "Added")))
 }
