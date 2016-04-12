c9a83f04a82a5e943627906fb2d29e813eae54a3
manicqin <8845802>
2016-04-12 15:03:19 +0300

    Iron::Router cannot be Composited... so I flattened the viewers

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -8,12 +8,14 @@ extern crate iron;
 extern crate mount;
 extern crate staticfile;
 extern crate router;
+extern crate persistent;
 
 use iron::prelude::*;
 use iron::AroundMiddleware;
 use mount::Mount;
 use staticfile::Static;
 use router::Router;
+use persistent::State;
 
 use xoxo_lib::game_logic::*;
 use xoxo_lib::utils::config::{Config};
@@ -60,36 +62,24 @@ fn main() {
      let mut sessions = game_sessions_handler::GamSessions::new();
      sessions.push(session);
 
-     let mut router = Router::new();
+      let mut main_router = Router::new();
+      main_router.get("game/hello".to_string(), hello_world::HelloWorld::new("shit got real".to_string()));
+      main_router.get("game/user", players_handler::auhentcate_player);
 
-     router.any("/submit_comment",comments_wrapper::handle_comments);
-     router.get("/comments.json",comments_wrapper::handle_comments);
+      let mut main_chain = Chain::new(main_router);
 
-     router.get("hello".to_string(), hello_world::HelloWorld::new("shit got real".to_string()));
-     router.get("/user", players_handler::auhentcate_player);
-
-     let mut chain = Chain::new(router);
-
-     //chain.link_before(persistent::State::<players_handler::PlayersHandler>::one(roster));
-     //chain.link_before(persistent::State::<game_sessions_handler::GamSessions>::one(sessions));
-     chain.link_before(players_handler::PlayersHandlerAuthenticator);
-     chain.link_before(game_sessions_handler::GameSessionFinder);
+      main_chain.link_before(persistent::State::<players_handler::PlayersHandler>::one(roster));
+      main_chain.link_before(persistent::State::<game_sessions_handler::GamSessions>::one(sessions));
+      main_chain.link_before(players_handler::PlayersHandlerAuthenticator);
+      main_chain.link_before(game_sessions_handler::GameSessionFinder);
 
       let mut mount = Mount::new();
       mount.mount("/scripts/",Static::new(scripts_path));
       mount.mount("/static-pages/",Static::new(static_path.clone()));
       mount.mount("/favicon.ico",Static::new(static_path + "favicon.ico"));
-
-      mount.mount("/",chain);
+      mount.mount("/",main_chain);
 
      //curl -i "localhost:6767/" -H "application/json" -d '{"author":"jason","text":"2kjhkjhjh kjh khkj kj hkjh kh kmhghg jjhghg fhg 98654345678987654"}'
-
      let server = Iron::new(web_logger::Logger::new(logger_mode).around(Box::new(mount)));
      server.http(format!("{}:{}",ip,port).trim()).unwrap();
-
-    //  let server = Iron::new(|_: &mut Request|{
-    //      Ok(Response::with((status::Ok,"hello")))}).http("localhost:3000").unwrap();
-
-    //let server = Iron::new(comments_wrapper::handle_comments).http("localhost:3000").unwrap();
-
 }
