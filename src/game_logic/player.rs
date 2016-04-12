971e586ee1a7272ee5bd0a7bb2afa2c82fb7e0d4
manicqin <8845802>
2016-04-12 14:58:14 +0300

    SUpport oredering for Player

--- src/game_logic/player.rs
+++ src/game_logic/player.rs
@@ -1,3 +1,5 @@
+use std::cmp::Ordering;
+
 #[derive(Clone, Copy, Debug)]
 pub struct Player{
         id:u32
@@ -11,8 +13,37 @@ impl Player{
     pub fn get_id(&self)->u32{
         self.id
     }
+    
+    pub fn get_id_ref(&self)->&u32{
+        &self.id
+    }
 }
 
+impl Ord for Player{
+    fn cmp(&self, other: &Player) -> Ordering{
+        self.id.cmp(&other.id)            
+    }
+}
+
+impl PartialOrd for Player{
+    fn partial_cmp(&self, other: &Player) -> Option<Ordering>{
+        self.id.partial_cmp(&other.id)
+    }
+}
+
+
+impl Eq for Player{
+}
+
+impl PartialEq for Player{
+    fn eq(&self, other :&Player) -> bool{
+        self.id.eq(&other.id)
+    }
+    
+    fn ne(&self, other :&Player) -> bool{
+        self.id.ne(&other.id)
+    }
+}
 // pub struct PlayerRegistration{
 //     players_hub:Vec<Player>
 // }
